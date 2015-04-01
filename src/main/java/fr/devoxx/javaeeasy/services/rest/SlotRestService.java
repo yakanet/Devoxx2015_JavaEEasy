5e1b0b68ac99d0233192bec9dfbd7ad6f1f8bfd7
Yann Blazart <yann.blazart@bycode.fr>
2015-04-01 18:18:44 +0200

    Ajout du slotrestservicetest

--- src/main/java/fr/devoxx/javaeeasy/services/rest/SlotRestService.java
+++ src/main/java/fr/devoxx/javaeeasy/services/rest/SlotRestService.java
@@ -1,28 +1,13 @@
 package fr.devoxx.javaeeasy.services.rest;
 
+import fr.devoxx.javaeeasy.models.cfp.Slot;
+
 import java.util.Collection;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import fr.devoxx.javaeeasy.models.cfp.Slot;
-import fr.devoxx.javaeeasy.services.business.DevoxxClient;
-
-@ApplicationScoped
-@Path("conferences")
 public class SlotRestService {
 
-    @Inject
-    private DevoxxClient devoxxClient;
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Slot> getConferences(){
-        return devoxxClient.retrieveSlots();
+    public Collection<Slot> getConferences() {
+        return null;
     }
 
 }
--- src/test/fr/devoxx/javaeeasy/services/rest/SlotRestServiceTest.java
+++ src/test/fr/devoxx/javaeeasy/services/rest/SlotRestServiceTest.java
@@ -1,7 +1,28 @@
 package fr.devoxx.javaeeasy.services.rest;
 
-/**
- * Created by M07800 on 01/04/2015.
- */
-public class SlotRestServiceTest {
+import fr.devoxx.javaeeasy.mock.BaseTestConfig;
+import fr.devoxx.javaeeasy.models.cfp.Slot;
+import org.apache.johnzon.jaxrs.JohnzonProvider;
+import org.junit.Test;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import static fr.devoxx.javaeeasy.mock.DevoxxServerMock.assertConferences;
+
+public class SlotRestServiceTest extends BaseTestConfig {
+    @Test
+    public void getConferences() {
+        assertConferences(
+                ClientBuilder.newBuilder().build()
+                        .register(new JohnzonProvider())
+                        .target(container.getInstance(Container.class).getRoot().toExternalForm() + "app/rest/conferences")
+                        .request(MediaType.APPLICATION_JSON_TYPE)
+                        .get(new GenericType<List<Slot>>() {
+                        }));
+    }
+
+
 }

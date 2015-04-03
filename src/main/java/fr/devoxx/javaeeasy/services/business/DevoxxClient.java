b71c9ff2e550d24725ae6711e13ed352b135a837
Romain Manni-Bucau <rmannibucau@apache.org>
2015-04-03 09:20:24 +0200

    useless imports

--- src/main/java/fr/devoxx/javaeeasy/services/business/DevoxxClient.java
+++ src/main/java/fr/devoxx/javaeeasy/services/business/DevoxxClient.java
@@ -1,20 +1,8 @@
 package fr.devoxx.javaeeasy.services.business;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-
-import fr.devoxx.javaeeasy.models.cfp.Link;
-import fr.devoxx.javaeeasy.models.cfp.LinksSchema;
 import fr.devoxx.javaeeasy.models.cfp.Slot;
-import fr.devoxx.javaeeasy.models.cfp.SlotsSchema;
+
+import java.util.List;
 
 
 public class DevoxxClient {

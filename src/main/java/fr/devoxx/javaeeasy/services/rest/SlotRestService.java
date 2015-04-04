b71f9f2499c5e272f86b2700ad5e09d001cd768e
yann.blazart@bycode.fr <yann.blazart@bycode.fr>
2015-04-04 14:55:28 +0200

    retrait des element inutile de la correction

--- src/main/java/fr/devoxx/javaeeasy/services/rest/SlotRestService.java
+++ src/main/java/fr/devoxx/javaeeasy/services/rest/SlotRestService.java
@@ -30,10 +30,5 @@ public class SlotRestService {
 		return conferences.getConferences();
 	}
 
-	@GET
-	@Path("attendees")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Map<String,Integer> getAttendeesBySlotId(){
-		return slotService.getAttendeesCountGroupedBySlotId();
-	}
+
 }
